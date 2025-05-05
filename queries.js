7c6c4b30d34da681a0b1807ac735eefdc971ecc2
AnonDrew <segdrew328@gmail.com>
2025-05-05 10:59:41 -0400

    Add getFavLinks function to retrieve favorite links from the database

--- queries.js
+++ queries.js
@@ -9,3 +9,15 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
 })
+
+const getFavLinks = (req, res) => {
+  pool.query('SELECT * FROM favlinks', (error, result) => {
+    if (error) {
+
+    }
+    else {
+      console.log(result)
+      res.status(200).json(result.rows)
+    }
+  })
+}
